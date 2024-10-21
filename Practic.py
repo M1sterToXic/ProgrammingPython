aa5f744c14fd5949cf91f636a21b655d93937038
M1ster.ToxiC <krivolapovnikita9@gmail.com>
2024-10-21 19:38:57 +0700

    Add files via upload

---
+++ Practic.py
@@ -0,0 +1,70 @@
+# from random import randint
+# n = 100
+# a = [0] * n
+# for i in range(n):
+#     a[i] = randint(-5000, 5000)
+#     print(a[i], end='  ')
+# min = a[0]
+# for i in range(len(a)):
+#     if ((a[i]%9==0) and (a[i]<min)):
+#         min = a[i]
+#         print(a[i])
+#         print(min)
+#     else:
+#         print(0)
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# from random import randint
+# n = 10
+# a = [0] * n
+# for i in range(n):
+#     a[i] = randint(0, 100)
+#     if (a[i]%2==0):
+#         print(a[i])
+#     elif (a[i]%4==0):
+#         print(a[i])
+#     elif (a[i]==0):
+#         print(a[i])
+# print(sorted(a))
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# from random import randint
+# n = 15
+# a = [0] * n
+# m = 0
+# for i in range(n):
+#     a[i] = randint(0, 10000)
+#     if (a[i]%2==0):
+#         m += a[i]
+#         print(m)
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# from random import randint
+# import random
+# k =15
+# a = [random.randint(0,10) for i in range(int(input("Long mas: ")))]
+# print(sorted(a))
+# # min = a[0]
+# # for i in range(len(a)):
+# #     if a[i] < min:
+# #         min = a[i]
+# # print(f"min: {min}")
+# for i in range(len(a)):
+#     m = a[0]
+#     if m + a[i] == k:
+#         print(m, a[i])
+#         m = a[i + 1]
+#     else:
+#         print("no")
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+
+
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
